34aa6e32af47ff99fe6d5b29b80e981903fa3657
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2022-12-28 14:32:44 +0700

    delete fetch problem

--- pages/dashboard/anggota/index.vue
+++ pages/dashboard/anggota/index.vue
@@ -2,20 +2,12 @@
 definePageMeta({
   layout: "dashboard",
 });
-
-const { data: fetchRepo } = await useFetch(
-  "https://repository.unpad.ac.id/custom/dirtakel",
-  {
-    mode: "no-cors",
-  }
-);
 </script>
 
 <template>
   <section>
     <h1>Heyo this is anggota</h1>
     <NuxtLink to="/dashboard">Back to Dashboard</NuxtLink>
-    <p>{{ fetchRepo }}</p>
   </section>
 </template>
 
