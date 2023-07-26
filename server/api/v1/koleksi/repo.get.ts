499509c5acaa099d828e791c06c6e04647307392
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2023-07-26 08:18:16 +0700

    example of dcmi

--- server/api/v1/koleksi/repo.get.ts
+++ server/api/v1/koleksi/repo.get.ts
@@ -4,13 +4,13 @@ export default defineEventHandler(async (event) => {
     jenisKoleksi: "Karya Ilmiah",
     results: [
       {
-        judul:
+        "dc:title":
           "RANCANG BANGUN MEDIA INFORMASI UNTUK PERPUSTAKAAN PUSAT UNIVERSITAS PADJADJARAN",
-        author: "Chrisna Adhi Pranoto",
-        tipeKoleksi: "Skripsi - S1",
-        fakultas: "Fakultas Ilmu Komunikasi",
-        prodi: "Perpustakaan & Sains Informasi",
-        deskripsi: `Perpustakaan merupakan sebuah lembaga yang menjadi pusat dari sumber bahan
+        "dc:creator": "Chrisna Adhi Pranoto",
+        "dc:type": "Undergraduate Thesis",
+        "dc:publisher": "Fakultas Ilmu Komunikasi",
+        "dc:contributor": "Perpustakaan & Sains Informasi",
+        "dc:description": `Perpustakaan merupakan sebuah lembaga yang menjadi pusat dari sumber bahan
         pustaka ilmiah dan informasi, sehingga sudah menjadi hal yang sangat penting
         bagi Perpustakaan untuk memiliki media informasi yang sangat adaptif dan efektif
         untuk digunakan dalam proses diseminasi dan publikasi informasi. Perpustakaan
@@ -30,56 +30,50 @@ export default defineEventHandler(async (event) => {
         kebijakan yang ketat dalam hal pengoperasiannya, sehingga diperlukan kerjasama
         dari berbagai pihak agar proses publikasi dan diseminasi informasi dapat
         dilakukan dengan baik dan tepat sasaran`,
-        cover: "",
       },
       {
-        judul:
+        "dc:title":
           "TINJAUAN YURIDIS MENGENAI PERBUATAN PENYEBARAN BERITA BOHONG MELALUI BEBERAPA MEDIA DALAM PERSPEKTIF PENEGAKAN HUKUM",
-        author: "Destha Ayudia Nurainia",
-        tipeKoleksi: "Skripsi - S1",
-        fakultas: "Fakultas Hukum",
-        prodi: "Ilmu Hukum",
-        deskripsi: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste sint dignissimos eius blanditiis sed! Quos iste enim saepe earum ullam, dicta explicabo nemo tempore, est quo ut. Saepe provident minima atque placeat quia sed corrupti exercitationem illo voluptas numquam alias tempore eos debitis eveniet accusamus, possimus explicabo? Ad ratione a atque, tempora, explicabo tempore veritatis illum dolore magnam enim, sequi quas impedit ipsum quam. Ad distinctio, qui expedita libero fugiat placeat voluptas, ullam hic dolores assumenda rerum doloribus necessitatibus iste. Ratione, labore nisi, exercitationem dicta earum officiis dolore autem soluta, error dolores debitis necessitatibus inventore laborum? Ratione fugit autem voluptates.`,
-        cover: "",
+        "dc:creator": "Destha Ayudia Nurainia",
+        "dc:type": "Undergraduate Theses",
+        "dc:publisher": "Fakultas Hukum",
+        "dc:contributor": "Ilmu Hukum",
+        "dc:description": `Peristiwa yang menjadi pusat perhatian media, adanya seseorang yang mengakui dan menjadikan dirinya sebagai penerus kerajaan yang sudah lama runtuh, dan mempunyai keyakinan bahwa NKRI sudah tidak dapat dijadikan sebagai kedaulatan sebuah Negara, dan ada yang memiliki keyakinan bahwa Persatuan Bangsa Bangsa lahir di Bandung, peristiwa tersebut diantaranya adalah berdirinya Sunda Empire, Kerajaan Rakyat Nusantara, dan Keraton Agung Sejagad. Adanya struktur organisasi dalam kerajaan tersebut dan banyaknya anggota di dalamnya. Mengingat, sistem pemerintahan Negara Indonesia berbentuk republik, sehingan peristiwa ini sangat meresahkan. Metode penulisan yang digunakan adalah melalui metode pendekatan yuridis normatif, yaitu meneliti peraturan hukum dan literatur berkaitan dengan pembahasan yang diteliti. Penulisan hukum ini menggunakan spesifikasi penulisan deskriptif analitis yang menggambarkan permasalahan didasarkan pada data-data yang ada, lalu dianalisa lebih lanjut untuk kemudian diambil sebuah kesimpulan. Hasil Penelitian ini menunjukan bahwa, setelah terkuaknya kasus kasus penyebaran berita bohong seperti pendirian Negara Kesatuan Nusantara dan sejenisnya, memperlihatkan bahwa masih banyak dan maraknya berita-berita bohong yang menyebar yang mengakibatkan keresahan dan keonaran di dalam kehidupan masyarakat. Dan dalam upaya hukum untuk mencegak dan menindaknya dinilai masih kurang karena banyaknya faktor-faktor penghambat yang dirasa sangat berpengaruh besar dalam menyikapi berita-berita bohong yang dapat menggiring opini publik dan membuat sebagian lapisan masyarakt awam mempercayai berita bohong tersebut. Namun dalam penerapan Pasal 14 dan 15 Undang-Undang nomor 1 tahun 1946 tentang Pengaturan Hukum Pidana sudah secara tepat dikenakan kepada pelaku perbuatan penyebaran berita bohong.`,
       },
       {
-        judul:
-          "Pengobatan Diabetes Mellitus Tipe 2 Sindrom Shang Xlao Dengan Akunpuntur Dan Herbal Sambiloto (Andrographis Paniculata Ness)",
-        author: "Ferry Puji Fornomo",
-        tipeKoleksi: "Tugas Akhir - D4",
-        fakultas: "Fakultas Farmasi",
-        prodi: "Sarjana Terapan Farmasi",
-        deskripsi: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste sint dignissimos eius blanditiis sed! Quos iste enim saepe earum ullam, dicta explicabo nemo tempore, est quo ut. Saepe provident minima atque placeat quia sed corrupti exercitationem illo voluptas numquam alias tempore eos debitis eveniet accusamus, possimus explicabo? Ad ratione a atque, tempora, explicabo tempore veritatis illum dolore magnam enim, sequi quas impedit ipsum quam. Ad distinctio, qui expedita libero fugiat placeat voluptas, ullam hic dolores assumenda rerum doloribus necessitatibus iste. Ratione, labore nisi, exercitationem dicta earum officiis dolore autem soluta, error dolores debitis necessitatibus inventore laborum? Ratione fugit autem voluptates.`,
-        cover: "",
+        "dc:title":
+          "Perbedaan Ambang Rasa Manis pada Individu Dengan dan Tanpa Riwayat Keluarga Diabetes Melitus Tipe 2 di Kecamatan Jatinangor",
+        "dc:creator": "Frisalia Briliantika Dini",
+        "dc:type": "Undergraduate Theses",
+        "dc:publisher": "Fakultas Kedokteran",
+        "dc:contributor": "Sarjana Kedokteran",
+        "dc:description": `Latar belakang: Individu dengan riwayat keluarga Diabetes Melitus Tipe 2 (DMT2) memiliki risiko lebih tinggi mengalami DMT2. Pencegahan primer DMT2 dapat dilakukan dengan membatasi konsumsi gula. Rasa merupakan faktor penting yang mempengaruhi konsumsi makanan. Ambang rasa manis mengukur sensitivitas rasa manis, yang jika meningkat menandakan penambahan jumlah gula yang dibutuhkan untuk menghasilkan rasa yang memuaskan. Penelitian ini bertujuan untuk mengetahui apakah terdapat perbedaan ambang batas rasa manis yang signifikan antara individu dengan dan tanpa riwayat keluarga DMT2. Metode: Penelitian analitik observasional dengan desain kasus kontrol dilakukan di Kecamatan Jatinangor. Sebanyak 19 dan 31 individu dengan dan tanpa riwayat keluarga DMT2 diikutkan dalam penelitian berdasarkan kriteria inklusi dan eksklusi, menggunakan non-probability sampling. Pengukuran ambang rasa manis terhadap larutan glukosa dilakukan menggunakan metode 3-Alternative Forced Choice. Data ambang rasa manis dianalisis menggunakan uji Chi-Square dengan level signifikansi 0,05. Hasil: Perbedaan ambang rasa manis antara individu dengan dan tanpa riwayat keluarga DMT2 tidak signifikan, namun rerata geometri ambang rasa manis kelompok dengan riwayat keluarga DMT2 lebih tinggi (89,1 mM vs 82 mM). Kesimpulan: Ambang rasa manis pada kelompok dengan riwayat keluarga DMT2 lebih tinggi dibandingkan kelompok tanpa riwayat, meskipun perbedaan tersebut tidak bermakna secara statistik. Penelitian ini menekankan salah satu program intervensi gaya hidup untuk mencegah T2DM yaitu dengan membatasi konsumsi gula.`,
       },
       {
-        judul:
+        "dc:title":
           "Variasi Pola Dermatoglifik, Jumlah Rigi Total, Dan Tingkat Bilateral Asimetri Dermatoglifik Monyet Ekor Panjang (Macaca fascicularis)Yang Hidup Di Beberapa Tempat Di Bali",
-        author: "I Gede Soma",
-        tipeKoleksi: "Tesis - S2",
-        fakultas: "Fakultas Kedokteran",
-        prodi: "Magister Ilmu Kedokteran",
-        deskripsi: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste sint dignissimos eius blanditiis sed! Quos iste enim saepe earum ullam, dicta explicabo nemo tempore, est quo ut. Saepe provident minima atque placeat quia sed corrupti exercitationem illo voluptas numquam alias tempore eos debitis eveniet accusamus, possimus explicabo? Ad ratione a atque, tempora, explicabo tempore veritatis illum dolore magnam enim, sequi quas impedit ipsum quam. Ad distinctio, qui expedita libero fugiat placeat voluptas, ullam hic dolores assumenda rerum doloribus necessitatibus iste. Ratione, labore nisi, exercitationem dicta earum officiis dolore autem soluta, error dolores debitis necessitatibus inventore laborum? Ratione fugit autem voluptates.`,
-        cover: "",
+        "dc:creator": "I Gede Soma",
+        "dc:type": "Theses",
+        "dc:publisher": "Fakultas Kedokteran",
+        "dc:contributor": "Magister Ilmu Kedokteran",
+        "dc:description": `Monyet ekor panjang (Macaca jascicularis) di Bali hidup dalam kelompok-kelompok di beberapa tempat seperti di Sangeh, Ubud, Alas Kedaton, Pulaki, dan Uluwatu. Antar kelompok yang satu dengan yang lain saling terisolasi oleh jarak geografis dan perkempungan penduduk disekeliling habitatnya. Selama ini Macaca jascicularis yang ada di Bali hanya dijadikan obyek wisata dan pengelolaannya dilakukan oleh masyarakat desa adat setempat. Cara perlakuan yang kurang tepat oleh wisatawan dan manajemen pengelola, tingkat kepadatan populasi, dan daya dukung lingkungan yang kurang memadai sangat potensial menjadi sumber cekaman (stressor) pada Macaca fascicularis tersebut. Macaca jascicularis merupakan salah satu hewan model dalam penelitian biomedis Disamping sebagai obyek wisata Macaca fascicularis yang ada di Bali sangat potensial dikembangkan sebagai sumber hewan model dalam penelitian biomedis. . Indonesia merupakan salah satu negara pengekspor Macaca jascicularis. Kebutuhan Macaca jascicularis terns meningkat. Untuk dapat memanfaatkan dan mengembangkan fungsi serta pelestarian Macaca fascicularis yang ada di Bali, sangat diperlukan informasi ilmiah yang bersumber dari hasil penelitian tentang satwa ini. Penelitian tentang Macaca fascicularis belum banyak dilakukan.`,
       },
       {
-        judul:
+        "dc:title":
           "PENGARUH FAKTOR EKONOMIS, PSIKOLOGIS, LINGKUNGAN KERJA, MOTIVASI, DAN RELIGIUSITAS ISLAMI TERHADAP KEPUTUSAN INVESTASI MANAJER TRESURI PERBANKAN ISLAM PADA PASAR UANG ANTAR BANK BERDASARKAN PRINSIP SYARIAH",
-        author: "IWAN WISANDANI",
-        tipeKoleksi: "Disertasi - S3",
-        fakultas: "Fakultas Ekonomi & Bisnis",
-        prodi: "Doktor Ilmu Ekonomi",
-        deskripsi: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste sint dignissimos eius blanditiis sed! Quos iste enim saepe earum ullam, dicta explicabo nemo tempore, est quo ut. Saepe provident minima atque placeat quia sed corrupti exercitationem illo voluptas numquam alias tempore eos debitis eveniet accusamus, possimus explicabo? Ad ratione a atque, tempora, explicabo tempore veritatis illum dolore magnam enim, sequi quas impedit ipsum quam. Ad distinctio, qui expedita libero fugiat placeat voluptas, ullam hic dolores assumenda rerum doloribus necessitatibus iste. Ratione, labore nisi, exercitationem dicta earum officiis dolore autem soluta, error dolores debitis necessitatibus inventore laborum? Ratione fugit autem voluptates.`,
-        cover: "",
+        "dc:creator": "IWAN WISANDANI",
+        "dc:type": "Dissertation",
+        "dc:publisher": "Fakultas Ekonomi & Bisnis",
+        "dc:contributor": "Doktor Ilmu Ekonomi",
+        "dc:description": `Penelitian ini bertujuan untuk memperoleh bukti secara empiris tentang : Pengaruh Faktor Ekonomis, Psikologis, Lingkungan Kerja, Motivasi dan Religiusitas Islami terhadap Keputusan Investasi Manajer Tresuri Perbankan Islam pada Pasar Uang Antarbank Berdasarkan Prinsip Syariah. Metode penelitian menggunakan pendekatan kuantitatif, disertai dengan analisis syar’i. Populasi penelitian adalah Manajer Tresuri Perbankan Islam sebanyak 34 orang yang semuanya dijadikan sampel. Teknik pengumpulan data dengan menyebarkan kuesioner dengan menggunakan skala Likert. Analisis data menggunakan Structural Equation Modelling (SEM) dengan jenis Partial Least Squares (PLS). Dalam menguji instrumen penelitian menggunakan uji validitas dan uji reliabilitas, sedangkan untuk menguji hipotesis menggunakan analisis jalur (path analysis). Hasil penelitian ini menunjukkan bahwa variabel-variabel : 1). Faktor Ekonomis, Psikologis, Lingkungan kerja, dan Motivasi tidak berpengaruh terhadap keputusan Manajer Tresuri Perbankan Islam. 2). Faktor Religiusitas Islami berpengaruh terhadap keputusan Manajer Tresuri Perbankan Islam. Dengan demikian, Religiusitas Islami merupakan satu-satunya variabel yang berpengaruh terhadap keputusan investasi Manajer Tresuri Perbankan Islam dalam melakukan transaksi di Pasar Uang Antarbank Berdasarkan Prinsip Syariah.`,
       },
       {
-        judul: "Desain Flowchart Sistem Akuntansi Penggajian Pada CV. Gun",
-        author: "Daniel Ageng Indianto",
-        tipeKoleksi: "Tugas Akhir - D3",
-        fakultas: "Fakultas Ekonomi & Bisnis",
-        prodi: "Diploma Akuntansi",
-        deskripsi: `Kesimpulan yang dapat diambil berdasarkan hasil pembahasan sistem penggajian CV. GUN, adalah: 1. CV. GUN menerapkan penggajian secara bulanan dengan jumlah gaji tetap setiap bulannya. 2. Bagian atau fungsi yang terkait pada sistem penggajian CV. GUN adalah bagian administrasi dan operasional, bagian keuangan, dan direktur. Ketiga bagian ini dianggap belum sesuai karena tidak ada bagian yang secara khusus mengatur soal penggajian. 3. Dokumen yang digunakan pada sistem penggajian CV. GUN adalah kartu absen, daftar absen pegawai, daftar gaji, bukti transfer, dan slip gaji. 4. Catatan akuntansi yang digunakan pada sistem penggajian CV. GUN adalah kartu penghasilan pegawai. 5. Jaringan prosedur yang digunakan pada sistem penggajian CV. GUN adalah prosedur pencatatan waktu hadir, prosedur pembuatan daftar gaji, dan prosedur pembayaran gaji. 6. Tidak ada nya kontrol atas jam hadir sampai jam pulang karyawan dapat mengakibatkan kerugian bagi CV. GUN, jika didapati ada karyawan yang curang dalam menyampaikan jam hadir sampai jam pulang. 7. Dokumen untuk mencatat setiap pengeluaran kas perusahaan belum maksimal sehingga pencatatan untuk kas keluar jadi kurang efektif dan dapat berdampak pada keputusan manajemen.`,
-        cover: "",
+        "dc:title": "Desain Flowchart Sistem Akuntansi Penggajian Pada CV. Gun",
+        "dc:creator": "Daniel Ageng Indianto",
+        "dc:type": "Undergraduate Thesis",
+        "dc:publisher": "Fakultas Ekonomi & Bisnis",
+        "dc:contributor": "Diploma Akuntansi",
+        "dc:description": `Kesimpulan yang dapat diambil berdasarkan hasil pembahasan sistem penggajian CV. GUN, adalah: 1. CV. GUN menerapkan penggajian secara bulanan dengan jumlah gaji tetap setiap bulannya. 2. Bagian atau fungsi yang terkait pada sistem penggajian CV. GUN adalah bagian administrasi dan operasional, bagian keuangan, dan direktur. Ketiga bagian ini dianggap belum sesuai karena tidak ada bagian yang secara khusus mengatur soal penggajian. 3. Dokumen yang digunakan pada sistem penggajian CV. GUN adalah kartu absen, daftar absen pegawai, daftar gaji, bukti transfer, dan slip gaji. 4. Catatan akuntansi yang digunakan pada sistem penggajian CV. GUN adalah kartu penghasilan pegawai. 5. Jaringan prosedur yang digunakan pada sistem penggajian CV. GUN adalah prosedur pencatatan waktu hadir, prosedur pembuatan daftar gaji, dan prosedur pembayaran gaji. 6. Tidak ada nya kontrol atas jam hadir sampai jam pulang karyawan dapat mengakibatkan kerugian bagi CV. GUN, jika didapati ada karyawan yang curang dalam menyampaikan jam hadir sampai jam pulang. 7. Dokumen untuk mencatat setiap pengeluaran kas perusahaan belum maksimal sehingga pencatatan untuk kas keluar jadi kurang efektif dan dapat berdampak pada keputusan manajemen.`,
       },
     ],
   };
