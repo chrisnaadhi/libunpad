0a003b0a7dc25fd9d7070f598983b29ae05093ad
chrisnaadhi <chrisnaadhip@gmail.com>
2024-09-06 21:00:52 +0700

    revised description

--- server/api/v1/tools.get.ts
+++ server/api/v1/tools.get.ts
@@ -3,28 +3,28 @@ export default defineEventHandler((event) => {
     {
       nama_tools: "Academic Tools",
       deskripsi:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum minus blanditiis cupiditate quidem deleniti mollitia cum aperiam nisi veniam?",
+        "Academic tools are software applications, platforms, and resources that support researchers in their work, including literature review, data collection, data analysis, writing, and collaboration.",
       icon: "i-mdi-spanner",
       slug: "/panduan",
     },
     {
       nama_tools: "Digital Collection",
       deskripsi:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga beatae et temporibus reiciendis hic fugit laboriosam dolores amet corrupti culpa!",
+        "Digital collections are organized assemblies of digital resources, such as articles, images, videos, and datasets, that are stored, preserved, and made accessible online for research, education, and other purposes.",
       icon: "i-mdi-pencil",
-      slug: "/panduan",
+      slug: "/koleksi",
     },
     {
       nama_tools: "Pintu Unpad",
       deskripsi:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate quam minima ad natus nobis itaque quibusdam ipsam labore voluptates impedit.",
+        "E-resources services provide online access to a vast range of digital materials, including e-books, articles, research papers, and other online content, through libraries, institutions, or online platforms, allowing users to remotely access and utilize these resources for research, education, and other purposes.",
       icon: "i-mdi-books",
       slug: "/panduan",
     },
     {
       nama_tools: "Study Assist",
       deskripsi:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam doloremque debitis quis rerum voluptas expedita molestiae, cum pariatur sed repudiandae.",
+        "Study assist refers to a range of services and resources provided by libraries, institutions, or online platforms to support students in their academic pursuits, including online tutorials, research guides, citation tools, and other learning aids, designed to help students develop their research, writing, and critical thinking skills.",
       icon: "i-mdi-settings",
       slug: "/panduan",
     },
