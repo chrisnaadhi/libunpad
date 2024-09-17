d3a795ee27c0f2650a0f651c1b98eb3b881a6196
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2024-09-17 09:40:22 +0700

    Update tools.get.ts

--- server/api/v1/tools.get.ts
+++ server/api/v1/tools.get.ts
@@ -3,28 +3,28 @@ export default defineEventHandler((event) => {
     {
       nama_tools: "Academic Tools",
       deskripsi:
-        "Academic tools are software applications, platforms, and resources that support researchers in their work, including literature review, data collection, data analysis, writing, and collaboration.",
+        "Software and resources aiding researchers in literature review, data collection, analysis, writing, and collaboration.",
       icon: "i-mdi-spanner",
       slug: "/panduan",
     },
     {
       nama_tools: "Digital Collection",
       deskripsi:
-        "Digital collections are organized assemblies of digital resources, such as articles, images, videos, and datasets, that are stored, preserved, and made accessible online for research, education, and other purposes.",
+        "Organized digital resources like articles, images, and videos, accessible online for research and education.",
       icon: "i-mdi-pencil",
       slug: "/koleksi",
     },
     {
       nama_tools: "Pintu Unpad",
       deskripsi:
-        "E-resources services provide online access to a vast range of digital materials, including e-books, articles, research papers, and other online content, through libraries, institutions, or online platforms, allowing users to remotely access and utilize these resources for research, education, and other purposes.",
+        "Online access to e-books, articles, and research papers through libraries or platforms for research and education.",
       icon: "i-mdi-books",
       slug: "/panduan",
     },
     {
       nama_tools: "Study Assist",
       deskripsi:
-        "Study assist refers to a range of services and resources provided by libraries, institutions, or online platforms to support students in their academic pursuits, including online tutorials, research guides, citation tools, and other learning aids, designed to help students develop their research, writing, and critical thinking skills.",
+        "Services and resources like tutorials, research guides, and citation tools to support students' academic skills.",
       icon: "i-mdi-settings",
       slug: "/panduan",
     },
