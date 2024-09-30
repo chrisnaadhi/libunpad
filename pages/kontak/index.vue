23a06a2c81f01ff20f14c5413f326303e20cf40d
chrisnaadhi <chrisnaadhip@gmail.com>
2024-09-30 23:02:30 +0700

    remove unused spinner

--- pages/kontak/index.vue
+++ pages/kontak/index.vue
@@ -132,27 +132,4 @@ input,
 textarea {
   --at-apply: border-1 border-orange rounded py-2 px-1;
 }
-
-/* HTML: <div class="loader"></div> */
-.loader {
-  --d: 22px;
-  width: 4px;
-  height: 4px;
-  border-radius: 50%;
-  color: #25b09b;
-  box-shadow: calc(1 * var(--d)) calc(0 * var(--d)) 0 0,
-    calc(0.707 * var(--d)) calc(0.707 * var(--d)) 0 1px,
-    calc(0 * var(--d)) calc(1 * var(--d)) 0 2px,
-    calc(-0.707 * var(--d)) calc(0.707 * var(--d)) 0 3px,
-    calc(-1 * var(--d)) calc(0 * var(--d)) 0 4px,
-    calc(-0.707 * var(--d)) calc(-0.707 * var(--d)) 0 5px,
-    calc(0 * var(--d)) calc(-1 * var(--d)) 0 6px;
-  animation: l27 1s infinite steps(8);
-}
-
-@keyframes l27 {
-  100% {
-    transform: rotate(1turn);
-  }
-}
 </style>
