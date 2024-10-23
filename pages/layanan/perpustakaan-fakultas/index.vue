2044a4adfb73b004037d78f8ce60947b8c13c0b5
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2024-10-23 15:52:32 +0700

    Update index.vue

--- pages/layanan/perpustakaan-fakultas/index.vue
+++ pages/layanan/perpustakaan-fakultas/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-const val = ref("Perpustakaan Fakultas");
+const { getItems } = useDirectusItems();
+
 const daftarLayananFakultas = [
   {
     id: 1,
@@ -26,11 +27,27 @@ const daftarLayananFakultas = [
     email: "",
   },
 ];
+
+const getJamLayananPerpustakaan = await getItems({
+  collection: "jam_layanan_perpustakaan",
+  params: {
+    filter: {
+      status: "published",
+    },
+  },
+});
 </script>
 
 <template>
   <section>
-    <p>{{ val }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+      vero!
+    </p>
+    <div>
+      <h2>Layanan Perpustakaan Fakultas</h2>
+      <p></p>
+    </div>
   </section>
 </template>
 
