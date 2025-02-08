4181b558d3506dd62dd97cb6cd966efd3aa00110
chrisnaadhi <chrisnaadhip@gmail.com>
2025-02-08 12:34:46 +0700

    informasi ruang alh

---
+++ pages/kandaga/fasilitas/ruang_alh/index.vue
@@ -0,0 +1,122 @@
+<script setup>
+definePageMeta({
+  middleware: ["authentication-check"],
+});
+
+const { getItems } = useDirectusItems();
+const getAllDataBarangALH = await getItems({
+  collection: "data_barang_alh",
+});
+
+const getJumlahDataBarang = getAllDataBarangALH.reduce((acc, item) => {
+  acc[item.kategori_spesifik] = (acc[item.kategori_spesifik] || 0) + 1;
+  return acc;
+}, {});
+
+console.log(getJumlahDataBarang);
+</script>
+
+<template>
+  <section>
+    <div class="text-center my-5">
+      <h1>Daftar Barang ALH</h1>
+      <p>Daftar barang yang ada di ruang ALH</p>
+      <pre>{{ getJumlahDataBarang }}</pre>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div
+        v-for="barang in getAllDataBarangALH"
+        :key="barang.id"
+        class="bg-orange-50 rounded-xl p-5"
+      >
+        <p
+          class="bg-orange text-white max-w-40 ma text-center rounded-xl py-1 px-5"
+        >
+          #{{ barang.nomor_asset }}
+        </p>
+        <h4 class="text-center my-5">{{ barang.deskripsi }}</h4>
+        <div
+          :class="
+            barang.status_barang === 'tersedia'
+              ? 'bg-green-500 text-white'
+              : barang.status_barang === 'dipinjam'
+              ? 'bg-yellow-600 text-white'
+              : 'bg-gray-5 text-dark'
+          "
+          class="p-3 rounded-xl text-center"
+        >
+          {{ barang.status_barang }}
+        </div>
+        <div class="grid grid-cols-2 gap-2 my-5">
+          <div>
+            <p>Dipinjam Oleh :</p>
+            <h6>{{ barang.pengguna ?? "-" }}</h6>
+          </div>
+          <div>
+            <p>Penanggungjawab :</p>
+            <h6>{{ barang.penanggung_jawab }}</h6>
+          </div>
+          <div>
+            <p>Dipinjam pada :</p>
+            <h6>
+              {{ barang.tanggal_peminjaman ?? "-" }}
+            </h6>
+          </div>
+          <div>
+            <p>Dikembalikan pada :</p>
+            <h6>{{ barang.tanggal_pengembalian ?? "-" }}</h6>
+          </div>
+        </div>
+        <div>
+          <h6>Metadata Barang</h6>
+          <div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Kategori Spesifik</p>
+              <p class="col-span-2">: {{ barang.kategori_spesifik }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Kategori Asset</p>
+              <p class="col-span-2">: {{ barang.kategori_asset }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Tanggal Perolehan</p>
+              <p class="col-span-2">: {{ barang.tanggal_perolehan }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Supplier</p>
+              <p class="col-span-2">: {{ barang.supplier }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Harga</p>
+              <p class="col-span-2">: {{ barang.harga }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Unit Kerja</p>
+              <p class="col-span-2">: {{ barang.unit_kerja }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Gedung</p>
+              <p class="col-span-2">: {{ barang.gedung }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Kode Ruangan</p>
+              <p class="col-span-2">: {{ barang.kode_ruangan }}</p>
+            </div>
+            <div class="grid grid-cols-3 gap-2 text-sm">
+              <p>Nama Ruangan</p>
+              <p class="col-span-2">: {{ barang.nama_ruangan }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <pre>{{ getAllDataBarangALH }}</pre> -->
+  </section>
+</template>
+
+<style scoped>
+section {
+  --at-apply: max-w-7xl ma py-10;
+}
+</style>
