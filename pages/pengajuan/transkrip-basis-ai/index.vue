8970b02592f7c5388b7b9176223e45e11f443697
chrisnaadhi <chrisnaadhip@gmail.com>
2025-03-10 12:49:52 +0700

    add transkrip page

---
+++ pages/pengajuan/transkrip-basis-ai/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const val = ref("Transkrip Berbasis AI untuk Layanan Difabel");
+</script>
+
+<template>
+  <section>
+    <p>{{ val }}</p>
+    <p>Segera Hadir</p>
+    <p>Tertarik untuk menggunakannya ? silahkan hubungi :</p>
+    <a href="mailto:chrisna.adhi@unpad.ac.id">chrisna.adhi@unpad.ac.id</a>
+  </section>
+</template>
+
+<style scoped>
+section {
+  --at-apply: max-w-7xl ma text-center;
+}
+</style>
